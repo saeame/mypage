8e86e10e00730a5f4ebdce095d8a141212c5d345
saeame <saeame31@gmail.com>
2023-01-11 18:35:56 +0900

    comment (get, detail get, add, patch, delete)

---
+++ routes/comment.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const Comment = require('../schema/comment');
+
+router.get('/', async (req, res) => {
+  try {
+    const comments = await Comment.find().select('-password');
+    res.json({ data: comments });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/:postId', async (req, res) => {
+  const { postId } = req.params;
+
+  const { user, content, password } = req.body;
+  try {
+    const comment = await Comment.create({
+      user,
+      content,
+      password,
+      postId,
+    });
+    res.json({ data: comment });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/:postId', async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const comments = await Comment.find({ postId });
+    res.json({ data: comments });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { content, user, password } = req.body;
+
+  const comment = await Comment.findById(id);
+  if (!comment) {
+    return res.json({ message: '댓글이 조회되지 않습니다' });
+  }
+  const isPasswordCorrect = comment.password === password;
+  if (isPasswordCorrect) {
+    if (content) {
+      comment.content = content;
+    }
+    if (user) {
+      comment.content = content;
+    }
+    try {
+      const updatedComment = await comment.save();
+      res.json({ data: updatedComment });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.json({ message: '비밀번호가 틀렸습니다' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { password } = req.body;
+
+  const comment = await Comment.findById(id);
+  if (!comment) {
+    return res.json({ message: '댓글조회에 실패하였습니다' });
+  }
+
+  const isPasswordCorrect = (await Comment.password) === password;
+  if (isPasswordCorrect) {
+    try {
+      await comment.remove();
+      res.json({ message: '댓글을 삭제하였습니다' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    res.status(401).json({ message: '비밀번호가 틀립니다' });
+  }
+});
+
+module.exports = router;
